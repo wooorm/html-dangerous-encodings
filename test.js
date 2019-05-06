abdd1ac644c6c0d416b43ae50afbd630dc1dc374
Titus Wormer <tituswormer@gmail.com>
2019-05-06 15:54:31 +0200

    Fix code style

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 
 /* Dependencies. */
-var assert = require('assert');
-var test = require('tape');
-var htmlDangerousEncodings = require('.');
+var assert = require('assert')
+var test = require('tape')
+var htmlDangerousEncodings = require('.')
 
 /* Tests. */
-test('htmlDangerousEncodings', function (t) {
-  t.ok(Array.isArray(htmlDangerousEncodings), 'should be an `array`');
+test('htmlDangerousEncodings', function(t) {
+  t.ok(Array.isArray(htmlDangerousEncodings), 'should be an `array`')
 
-  t.doesNotThrow(function () {
-    htmlDangerousEncodings.forEach(function (encoding) {
-      assert(typeof encoding, 'string', '`' + encoding + '`');
-    });
-  }, 'should be all `string`s');
+  t.doesNotThrow(function() {
+    htmlDangerousEncodings.forEach(function(encoding) {
+      assert(typeof encoding, 'string', '`' + encoding + '`')
+    })
+  }, 'should be all `string`s')
 
-  t.end();
-});
+  t.end()
+})
