595a995f3dc6cb5017d1c3eb9626ff17647eb58e
Titus Wormer <tituswormer@gmail.com>
2020-10-26 20:11:30 +0100

    Update dev-dependencies

--- test.js
+++ test.js
@@ -4,11 +4,11 @@ var assert = require('assert')
 var test = require('tape')
 var htmlDangerousEncodings = require('.')
 
-test('htmlDangerousEncodings', function(t) {
+test('htmlDangerousEncodings', function (t) {
   t.ok(Array.isArray(htmlDangerousEncodings), 'should be an `array`')
 
-  t.doesNotThrow(function() {
-    htmlDangerousEncodings.forEach(function(encoding) {
+  t.doesNotThrow(function () {
+    htmlDangerousEncodings.forEach(function (encoding) {
       assert(typeof encoding, 'string', '`' + encoding + '`')
     })
   }, 'should be all `string`s')
