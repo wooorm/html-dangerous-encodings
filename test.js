a386beb83c0b8fe3c41da4bf4adfc85293078da6
Titus Wormer <tituswormer@gmail.com>
2022-11-16 13:36:10 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,21 +1,17 @@
-import assert from 'node:assert'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {htmlDangerousEncodings} from './index.js'
 
-test('htmlDangerousEncodings', function (t) {
-  t.ok(Array.isArray(htmlDangerousEncodings), 'should be an `array`')
+test('htmlDangerousEncodings', function () {
+  assert.ok(Array.isArray(htmlDangerousEncodings), 'should be an `array`')
 
-  t.doesNotThrow(function () {
-    let index = -1
+  let index = -1
 
-    while (++index < htmlDangerousEncodings.length) {
-      assert.equal(
-        typeof htmlDangerousEncodings[index],
-        'string',
-        '`' + htmlDangerousEncodings[index] + '`'
-      )
-    }
-  }, 'should be all `string`s')
-
-  t.end()
+  while (++index < htmlDangerousEncodings.length) {
+    assert.equal(
+      typeof htmlDangerousEncodings[index],
+      'string',
+      '`' + htmlDangerousEncodings[index] + '`'
+    )
+  }
 })
